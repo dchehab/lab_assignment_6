e575ff1f17a8a7832567cd296fffab72b4fe6534
dchehab <160252303+dchehab@users.noreply.github.com>
2024-02-23 10:18:15 -0500

    lab_assigment_6.c

--- lab_assigment_6.c
+++ lab_assigment_6.c
@@ -1,60 +1,17 @@
-#include <stdio.h>
+if (low > high) {
+        // Base case
+        return -1;
+    }
 
-int search(int numbers[], int low, int high, int value)
-{
-	return -1;
-}
+    int mid = low + (high - low) / 2; // Calculate middle index
 
-void printArray(int numbers[], int sz)
-{
-	int i;
-	printf("Number array : ");
-	for (i = 0;i<sz;++i)
-	{
-		printf("%d ",numbers[i]);
-	}
-	printf("\n");
-}
-
-int main(void)
-{
-	int i, numInputs;
-	char* str;
-	float average;
-	int value;
-	int index;
-	int* numArray = NULL;
-	int countOfNums;
-	FILE* inFile = fopen("input.txt","r");
-
-	fscanf(inFile, " %d\n", &numInputs);
-
-	while (numInputs-- > 0)
-	{
-		fscanf(inFile, " %d\n", &countOfNums);
-		numArray = (int *) malloc(countOfNums * sizeof(int));
-		average = 0;
-		for (i = 0; i < countOfNums; i++)
-		{
-			fscanf(inFile," %d", &value);
-			numArray[i] = value;
-			average += numArray[i];
-		}
-
-		printArray(numArray, countOfNums);
-		value = average / countOfNums;
-		index = search(numArray, 0, countOfNums - 1, value);
-		if (index >= 0)
-		{
-			printf("Item %d exists in the number array at index %d!\n",value, index);
-		}
-		else
-		{
-			printf("Item %d does not exist in the number array!\n", value);
-		}
-
-		free(numArray);
-	}
-
-	fclose(inFile);
-}
+    if (numbers[mid] == value) {
+        // Found the value at the middle index
+        return mid;
+    } else if (numbers[mid] < value) {
+        // Value is in the right half
+        return search(numbers, mid + 1, high, value);
+    } else {
+        // Value is in the left half
+        return search(numbers, low, mid - 1, value);
+    }
